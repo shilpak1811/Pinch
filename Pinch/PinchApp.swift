09c5a0ac1a563891b2b2bc320ba26df1a63ee65f
Shilpa Kumari <shilpa.kumari@gojek.com>
2023-04-02 12:27:37 +0530

    Initial Commit

---
+++ Pinch/PinchApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinchApp.swift
+//  Pinch
+//
+//  Created by Shilpa Kumari on 02/04/23.
+//
+
+import SwiftUI
+
+@main
+struct PinchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
